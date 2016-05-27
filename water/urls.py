a0433c7af7aa6f3fdfea8a49f32831eee912709c
anna <annateresavillanueva@gmail.com>
2016-05-28 00:15:49 +1000

    changed question url path to - (pattern path_to_home/question_id)

--- water/urls.py
+++ water/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^fire/', include('fire.urls')),
+    url(r'^/', include('fire.urls')),
     url(r'^admin/', admin.site.urls),
 ]
