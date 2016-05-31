08c6b93f1108dd75379eae80be25cc2488748561
anna <annateresavillanueva@gmail.com>
2016-05-31 21:56:21 +1000

    moved database requirements on settings.py file

--- water/settings.py
+++ water/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 import os
 import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +118,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
